794abb81b8d39e559190543571811eb64494e9fe
lenachat <124374456+lenachat@users.noreply.github.com>
2025-04-09 13:44:43 +0300

    mark affirmations as favorite
    - users can mark dailyn affirmations as favorite
    - favorite affirmations are saved as array in firestore within the user's collection

--- src/components/daily-affirmations/daily-affirmations.tsx
+++ src/components/daily-affirmations/daily-affirmations.tsx
@@ -1,8 +1,13 @@
 import { useEffect, useState } from "react";
-
+import { db } from "../../firebase";
+import { doc, updateDoc, arrayUnion, getDoc } from "firebase/firestore";
+import { v4 as uuidv4 } from "uuid"; // for generating random ids
 
 const DailyAffirmation = () => {
   const [affirmation, setAffirmation] = useState<string>("");
+
+  const userData = localStorage.getItem("user");
+  const userId = userData ? JSON.parse(userData).uid : null;
 
   useEffect(() => {
     const today = new Date().toISOString().split("T")[0]; // format: YYYY-MM-DD
@@ -33,9 +38,48 @@ const DailyAffirmation = () => {
     }
   }, []);
 
+  const saveToFavorites = async () => {
+
+    if (affirmation && userId) {
+      try {
+        const userDocRef = doc(db, "users", userId);
+        const userDoc = await getDoc(userDocRef);
+
+        if (userDoc.exists()) {
+          const favorites = userDoc.data().favoriteAffirmations || [];
+
+          // Check if affirmation already exists (by text)
+          const alreadyExists = favorites.some(
+            (fav: { affirmation: string }) => fav.affirmation === affirmation
+          );
+          if (alreadyExists) {
+            alert("Affirmation already in favorites!");
+            return;
+          }
+          const newAffirmation = {
+            id: uuidv4(),
+            affirmation,
+          };
+
+          await updateDoc(userDocRef, {
+            favoriteAffirmations: arrayUnion(newAffirmation),
+          });
+          alert("Affirmation added to favorites!");
+        }
+      } catch (error) {
+        console.error("Error saving favorite affirmation:", error);
+      }
+    }
+  };
+
+
   return (
     <>
       <div>{affirmation}.</div>
+      <button onClick={saveToFavorites} style={{ fontSize: "1.5rem", background: "none", border: "none", cursor: "pointer" }}>
+        {/* {isFavorite ? "⭐" : "☆"} */}
+        click me
+      </button>
     </>
   );
 };
